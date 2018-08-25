b50acda0f86e88de8b2eba544cf30afcd958924a
Artraitor <artraitor@gmail.com>
2018-08-24 21:19:50 -0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,34 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '.'
 
-client.on("ready", () => {
-  console.log("I am ready!");
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`ayyyyyy`,"http://twitch.tv/Respawn")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
 
-client.on("message", (message) => {
-  if (message.content.startsWith("مين اذكى زهراني؟")) {
-    message.channel.send("http://oi65.tinypic.com/qrhduq.jpg !");
-  }
-});
-
-
-client.login("BOT_TOKEN");
+client.login(process.env.BOT_TOKEN);
 
 
 client.on("message", async message => {
